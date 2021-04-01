b6a074b0f334abc213fc9a18dab4c6b813ef7a5d
Sebastian Pardo <sapardosuazo@gmail.com>
2021-03-31 21:43:43 -0300

    change setInterval for accurateInterval for timeout

--- public/worker.js
+++ public/worker.js
@@ -3,12 +3,52 @@ var interval;
 onmessage = (e) => {
   switch (e.data) {
     case "start":
-      interval = setInterval(() => postMessage(0), 1000);
+      // interval = setInterval(() => postMessage(0), 1000);
+      interval = accurateInterval(() => postMessage(0), 1000, {
+        inmediate: true,
+      });
       break;
     case "stop":
-      clearInterval(interval);
+      // clearInterval(interval);
+      interval.clear();
       break;
     default:
       break;
   }
 };
+
+function accurateInterval(func, interval, opts) {
+  if (!opts) opts = {};
+
+  var clear, nextAt, timeout, wrapper, now;
+
+  now = new Date().getTime();
+
+  nextAt = now;
+
+  if (opts.aligned) {
+    nextAt += interval - (now % interval);
+  }
+  if (!opts.immediate) {
+    nextAt += interval;
+  }
+
+  timeout = null;
+
+  wrapper = function wrapper() {
+    var scheduledTime = nextAt;
+    nextAt += interval;
+    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+    func(scheduledTime);
+  };
+
+  clear = function clear() {
+    return clearTimeout(timeout);
+  };
+
+  timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+
+  return {
+    clear: clear,
+  };
+}
